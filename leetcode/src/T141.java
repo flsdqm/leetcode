4c5b372de28756c33f6a3ba77a67440e944c8105
flsdqm <592661681@qq.com>
2022-09-23 15:22:05 +0800

    leetcode练习 T141. 环形链表

---
+++ leetcode/src/T141.java
@@ -0,0 +1,27 @@
+public class T141 {
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {//快慢指针
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;//让快指针跳一个读取，这样循环回来不一定一轮，但一定能追上
+        }
+        return true;
+    }
+}
