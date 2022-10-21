9f2f155ec9935fa2df93fad8b93e64ba44f53166
flsdqm <592661681@qq.com>
2022-10-21 08:40:27 +0800

    leetcode练习 T206. 反转链表

---
+++ leetcode/src/T206.java
@@ -0,0 +1,22 @@
+public class T206 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
