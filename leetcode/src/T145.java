fc1576f84b844d91bd3812f4c870891e81778c29
flsdqm <592661681@qq.com>
2023-01-10 15:20:00 +0800

    leetcode练习 T145. 二叉树的后序遍历

---
+++ leetcode/src/T145.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class T145 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    List<Integer> list = new LinkedList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if(root != null){
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            list.add(root.val);
+        }
+        return list;
+    }
+}
