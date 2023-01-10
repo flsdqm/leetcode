b02a803cfa49014623963b86e87b65e652192424
flsdqm <592661681@qq.com>
2023-01-10 15:07:21 +0800

    leetcode练习 T94. 二叉树的中序遍历

---
+++ leetcode/src/T94.java
@@ -0,0 +1,35 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class T94 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    List<Integer> list = new LinkedList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            list.add(root.val);
+            inorderTraversal(root.right);
+        }
+        return list;
+    }
+}
