17613bbffdcbba4a24fed744344dd5f39dd9dde5
flsdqm <592661681@qq.com>
2023-02-13 16:17:16 +0800

    leetcode练习 剑指 Offer 24. 反转链表

---
+++ leetcode/src/Offer24.java
@@ -0,0 +1,21 @@
+public class Offer24 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null, cur = head, next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
